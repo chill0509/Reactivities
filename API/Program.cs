6aeb81f2f4b7195c526fcda2cc75b03a28f3b8d9
Christina Decker <cdecker@americanmedical.com>
2023-10-27 12:02:15 -0500

    CORS Policy

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,10 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        //policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
     });
 });
 
